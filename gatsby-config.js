b427d3a82f2e467332647db1e4dcc967ca24f0ad
Ivan <58488559+separate-domains@users.noreply.github.com>
2019-12-12 23:28:15 -0600

    Tweak text

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require(`dotenv`).config()
 
 module.exports = {
   siteMetadata: {
-    title: `Vady cosmetics`,
+    title: `Vady cosmeticos`,
     description: `Cosmetics shop in Zihuatanejo, Guerrero`,
     author: `nn`,
   },
