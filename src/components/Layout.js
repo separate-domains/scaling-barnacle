e2494322c4582207d0b96aa73ae761c4cd773f07
Ivan <58488559+separate-domains@users.noreply.github.com>
2019-12-12 19:37:03 -0600

    Remove some compilation warnings

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
  Box,
  Grommet,
- Heading,
  ResponsiveContext,
  Text,
 } from 'grommet';
@@ -49,7 +48,7 @@ export const Layout = ({ children }) => (
       {size => (
         <>
           <AppBar>
-            <Text size='xxlarge'>Entregas personales en Ixtapa Zihuatanejo 🚘 Envíos a toda la República 📦 Calle AEIOU 12345, Colonia VWXYZ<br /><a href="https://facebook.com/vadycosmeticszihuatanejo"><img src='https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg' height='86px' /></a> <a href='https://wa.me/5217551065013'><img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" height='96px'/></a> <a href='https://goo.gl/maps/JTbF6mNiaYWW4MDE6'><img src='https://upload.wikimedia.org/wikipedia/commons/d/d1/Google_Maps_pin.svg' height='86px'/></a></Text>
+            <Text size='xxlarge'>Entregas personales en Ixtapa Zihuatanejo 🚘 Envíos a toda la República 📦 Calle AEIOU 12345, Colonia VWXYZ<br /><a href="https://facebook.com/vadycosmeticszihuatanejo"><img src='https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg' height='86px' alt='Facebook logo'/></a> <a href='https://wa.me/5217551065013'><img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" height='96px' alt='Whatsapp logo'/></a> <a href='https://goo.gl/maps/JTbF6mNiaYWW4MDE6'><img src='https://upload.wikimedia.org/wikipedia/commons/d/d1/Google_Maps_pin.svg' height='86px' alt='Google pin'/></a></Text>
           </AppBar>
           <Box fill>
             <Box flex>
